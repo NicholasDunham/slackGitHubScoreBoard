81152fbcac00c28b5e89b96527237570a7982e08
NicholasDunham <nmdnhm@gmail.com>
2017-06-14 16:09:58 -0700

    Remove 'hello' function

--- handler.js
+++ handler.js
@@ -2,21 +2,6 @@
 const GitHubApi = require("github");
 const moment = require("moment");
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: process.env.GITHUB_USERNAME,
-      input: event
-    })
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
 module.exports.singleUser = (event, context, callback) => {
   const weekStart = moment().startOf("day").subtract(1, "week");
 
@@ -26,19 +11,21 @@ module.exports.singleUser = (event, context, callback) => {
     token: process.env.GITHUB_TOKEN
   });
 
-  github.activity
-    .getEventsForUser({
-      username: "nicholasdunham"
+  github.repos
+    .getForUser({
+      username: "nicholasdunham",
+      type: "all",
+      sort: "updated",
+      direction: "desc",
+      per_page: 100
     })
-    .then(events => {
-      return events.data.filter(event =>
-        moment(event.created_at).isAfter(weekStart)
+    .then(repos => {
+      const recent = repos.data.filter(repo =>
+        moment(repo.updated_at).isAfter(weekStart)
       );
-    })
-    .then(events => {
       const response = {
         statusCode: 200,
-        body: JSON.stringify(events)
+        body: JSON.stringify(recent)
       };
 
       callback(null, response);
