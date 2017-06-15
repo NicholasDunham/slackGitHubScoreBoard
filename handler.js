7964b8142fb42e525a13c78163114a986f865d58
NicholasDunham <nmdnhm@gmail.com>
2017-06-15 02:39:17 -0700

    Output a score for single user

--- handler.js
+++ handler.js
@@ -2,29 +2,27 @@
 const GitHubApi = require("github");
 const moment = require("moment");
 
-const weekStart = moment().startOf("day").subtract(6, "month");
+const github = new GitHubApi();
+github.authenticate({
+  type: "oauth",
+  token: process.env.GITHUB_TOKEN
+});
+
+const weekStart = moment().startOf("day").subtract(1, "week");
 
 module.exports.singleUser = (event, context, callback) => {
-  const github = new GitHubApi();
-  github.authenticate({
-    type: "oauth",
-    token: process.env.GITHUB_TOKEN
-  });
-
-  github.repos
-    .getForUser({
-      username: "nicholasdunham",
-      type: "all",
-      sort: "updated",
-      direction: "desc",
-      per_page: 100
+  github.activity
+    .getEventsForUser({
+      username: "nicholasdunham"
     })
-    .then(filterRecentRepos)
-    .then(cleanUpRepos)
-    .then(repos => {
+    .then(filterRecentEvents)
+    .then(countEvents)
+    .then(finalScore => {
       const response = {
         statusCode: 200,
-        body: JSON.stringify([...repos])
+        body: JSON.stringify({
+          message: finalScore
+        })
       };
 
       callback(null, response);
@@ -33,23 +31,18 @@ module.exports.singleUser = (event, context, callback) => {
 
 // Helper Functions
 
-function filterRecentRepos(repos) {
-  return repos.data.filter(repo => {
-    return moment(repo.updated_at).isAfter(weekStart);
+function filterRecentEvents(events) {
+  return events.data.filter(event => {
+    return moment(event.updated_at).isAfter(weekStart);
   });
 }
 
-function cleanUpRepos(repos) {
-  return repos.map(repo => {
-    return {
-      id: repo.id,
-      name: repo.name,
-      owner: {
-        login: repo.owner.login,
-        id: repo.owner.id
-      },
-      html_url: repo.html_url,
-      updated_at: repo.updated_at
-    };
-  });
+function countEvents(events) {
+  return events.reduce((acc, event) => {
+    if (event.type === "PushEvent") {
+      return event.payload.commits.length + acc;
+    }
+
+    return acc + 1;
+  }, 0);
 }
