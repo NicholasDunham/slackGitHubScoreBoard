75dbd927c8af7ebe2f4217639131d992eabe8190
NicholasDunham <nmdnhm@gmail.com>
2017-06-16 01:18:26 -0700

    Better error handling, user lookup, and a bunch of other good stuff

--- handler.js
+++ handler.js
@@ -1,6 +1,8 @@
 "use strict";
 const GitHubApi = require("github");
 const moment = require("moment");
+const qs = require("querystring");
+const userTable = require("./userTable");
 
 const github = new GitHubApi();
 github.authenticate({
@@ -8,35 +10,56 @@ github.authenticate({
   token: process.env.GITHUB_TOKEN
 });
 
-const weekStart = moment().startOf("day").subtract(1, "week");
+const weekStart = moment().startOf("isoWeek");
+const helpText =
+  "To retrieve your own statistics, type `/scoreboard`.\nTo retrieve another user's statistics, use an @mention after the command, like this: `/scoreboard @nmdnhm`.\nNote that I can only handle one @mention at a time (for now).";
+const errorText =
+  "Sorry, either I didn't understand your command or I can't find that user. Please try again, or type `/scoreboard help` for options.";
 
 module.exports.singleUser = (event, context, callback) => {
-  github.activity
-    .getEventsForUser({
-      username: "nicholasdunham"
-    })
+  const params = qs.parse(event.body);
+  let slackName = params.text || params.user_name;
+
+  lookup(slackName)
     .then(filterRecentEvents)
     .then(countEvents)
-    .then(finalScore => {
-      // const response = {
-      //   statusCode: 200,
-      //   body: JSON.stringify({
-      //     text: `Your score: ${finalScore}`
-      //   })
-      // };
-
+    .then(score => {
+      return JSON.stringify({
+        response_type: "in_channel",
+        text: `So far this week, @${slackName} has earned ${score} points for contributions on GitHub.`
+      });
+    })
+    .catch(msg => {
+      return JSON.stringify({
+        text: msg
+      });
+    })
+    .then(msg => {
       const response = {
         statusCode: 200,
-        body: JSON.stringify({
-          text: `Your GitHub score for the past seven days: ${finalScore}`
-        })
+        body: msg
       };
-
       callback(null, response);
     });
 };
 
 // Helper Functions
+
+function lookup(slackName) {
+  if (slackName === "help") {
+    return Promise.reject(helpText);
+  }
+
+  slackName = slackName.replace(/^@/, "");
+
+  const gitHubName = userTable[slackName];
+
+  if (!gitHubName) {
+    return Promise.reject(errorText);
+  } else {
+    return github.activity.getEventsForUser({ username: gitHubName });
+  }
+}
 
 function filterRecentEvents(events) {
   return events.data.filter(event => {
