fec4d10bae6a41a0e3f310a3d6289a79d96e3b00
NicholasDunham <nmdnhm@gmail.com>
2017-06-26 02:44:29 -0700

    Add example secrets files

---
+++ userTable-example.ts
@@ -0,0 +1,10 @@
+interface IUsernameMap {
+  [key: string]: string;
+}
+
+const usernameMap: IUsernameMap = {
+  slackUser1: "GitHubUser1",
+  slackUser2: "GitHubUser2"
+};
+
+export default usernameMap;
